4b236d8b6d64eec1e05c1742a957b3c872a2e69d
L <401419353@qq.com>
2022-04-02 18:13:28 +0800

    增加本地化调用代码

--- Unity_Example/Assets/Scripts/View/UsageExample.cs
+++ Unity_Example/Assets/Scripts/View/UsageExample.cs
@@ -19,7 +19,7 @@ namespace Example
             new LocalizeComponent(
                 new LocalizeComponentConfig(
                     loader,
-                    SystemLanguage.ChineseSimplified,
+                    SystemLanguage.Chinese,
                     SystemLanguage.English,
                     null
                 )
@@ -28,16 +28,21 @@ namespace Example
             await LocalizeComponent.Instance.Load();
 
             // 单例表
-            Debug.Log(GlobalConfigCategory.Single.default_icon);
+            Debug.Log($"单例表: {GlobalConfigCategory.Single.default_icon}");
 
             // 多主键表
             var config = MultipleKeyConfigCategory.Instance.TryGet("First", 100, 200);
-            Debug.Log(config.ToString());
+            Debug.Log($"多主键表: {config}");
 
             // ref 绑定
             var ref_config = ConfigComponent.Instance.Get<SingleRefConfig>(1);
 
-            Debug.Log(ref_config.ref_one_ref.id);
+            Debug.Log($"ref 绑定: {ref_config.ref_one_ref.id}");
+
+            // 本地化使用
+            var localize = LocalizeComponent.Instance.GetText("example/one");
+
+            Debug.Log($"本地化: {localize}");
         }
     }
 }
